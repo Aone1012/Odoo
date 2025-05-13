b473dc041b2a88407e4244b32fb324823eac3a13
Aone 1 <arealflash@gmail.com>
2025-05-13 13:38:02 +0330

    Real estate model

---
+++ real_state_ads/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Real Estate Ads",
+    "version": "1.0",
+    "website": "https//www.odoo.com",
+    "author": "Aone",
+    "description": """
+    Real State module to show available properties
+    
+    """,
+    "category": "Sales",
+    "depends": [],
+    "data": [
+        'security/ir.model.access.csv'
+    ],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3"
+
+}
